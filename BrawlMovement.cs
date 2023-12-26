3a29b72970515172833c4046787c2aa81b6bb4d5
vo6i <deooor1r@gmail.com>
2023-12-26 03:07:10 +0300

    new scripts ready for born

---
+++ BrawlMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrawlMovement : MonoBehaviour
+{
+
+    [SerializedField]
+    Joystick joystick;
+
+    [SerializedField]
+    Transform playerSprite;
+
+    [SerializedField]
+    Animator animator
+
+    bool Movement;
+
+    void Update()
+    {
+        if(joystick.Horizontal != 0 || if(joystick.Vertical != 0)
+        {
+            playerSprite.position = new Vector3(joystick.Horizontal + transform.position.x, -1.54f,joystick.Vertical + transform.position.z);
+            transform.LookAt(new Vector3(playerSprite.position.x, 0, playerSprite.position.z));
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            if(animator.GetBool("Walking") != true)
+            {
+                animator.SetBool("Walking", true);
+            }
+            Movement = true;
+        }
+        else if(Movement == true)
+        {
+            animator.SetBool("Walking", false);
+            Movement = false;
+        }
+    }
+}
