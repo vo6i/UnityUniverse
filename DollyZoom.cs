791e87d998619cf0b5da5ceb75cf2d0ca597280d
vo6i <deooor1r@gmail.com>
2024-01-10 13:38:04 +0300

    new scripts ready for born

---
+++ DollyZoom.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DollyZoom: MonoBehaviour {
+
+    public Camera cam;
+    public Slider slider;
+
+    void Update () {
+        cam.fieldOfView = slider.value + 40;
+        cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 40, 140);
+        cam.gameObject.transform.position = new Vector3(cam.gameObject.transform.position, cam.gameObject.transform.position.y, slider.value * 0.13f - 19);
+    {
+
+}
